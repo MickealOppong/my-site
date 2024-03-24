7c57c63821a574e7ae9554c2835564d4ec1bc57e
yaw oppong <opp.mic@gmail.com>
2024-03-24 21:44:23 +0100

    css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
   return <header className="px-8 py-8 max-w-3xl mx-auto">
     <div className="flex items-center">
       <div className="flex flex-col">
-        <h1 className=" text-c-4 font-semibold text-7xl italic">Mike Oppong</h1>
+        <h1 className=" text-c-4 font-semibold text-6xl italic">Mike Oppong</h1>
         <div className="mt-2">
           <p className="capitalize text-gray-500">web developer</p>
           <ul className='flex gap-x-2 mt-4'>
