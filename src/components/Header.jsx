85fd8f41b025a19d1a55a4834f4a1df900ad12b6
yaw oppong <opp.mic@gmail.com>
2024-03-25 09:19:43 +0100

    cdd

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,15 +17,15 @@ const Header = () => {
   return <nav className={`px-8 py-2 ${isAbove ? "fixed top-0 left-0 right-0 bg-white z-50 flex justify-center" : 'max-w-3xl mx-auto'} `} ref={myHeadRef}>
     <div className="flex items-center">
       <div className="flex flex-col">
-        <h1 className=" text-c-4 font-semibold text-6xl italic">Mike Oppong</h1>
+        <h1 className=" text-c-4 font-semibold text-6xl italic tracking-wide">Mike Oppong</h1>
         <div className="mt-2">
-          <p className="capitalize text-gray-500 mt-1">web developer</p>
+          <p className="capitalize text-gray-500 mt-1 tracking-tight">web developer</p>
           <ul className='flex gap-x-2 mt-4'>
             {
               techStack.map((item) => {
                 return <div className='flex items-center gap-x-2' key={item.id}>
                   <div className='h-2 w-2 bg-c-2 '></div>
-                  <li className='uppercase tracking-wide text-xs text-slate-500'>{item.text}</li>
+                  <li className='tracking-tight uppercase tracking-wide text-xs text-slate-500'>{item.text}</li>
                 </div>
 
               })
