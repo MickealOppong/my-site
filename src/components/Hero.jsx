43ad72ba6d433147020e188e2b74be19946ab145
yaw oppong <opp.mic@gmail.com>
2024-03-25 10:47:12 +0100

    cdd

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,34 +1,42 @@
 import { useState } from 'react';
 import img1 from '../assets/mix-master.png';
+import img3 from '../assets/splash.png';
 import { page } from '../data';
 const Hero = () => {
   const [image, setImage] = useState(page[1].img)
 
-  return <section className="px-8 mt-8 md:px-16 lg:px-32 bg-gray-300 py-10 h-[90vh]">
+  return <section className="px-8 mt-8 md:px-16 lg:px-32 bg-gray-300 py-20 ">
 
-    <article className="grid gap-y-4 sm:grid-cols-2 sm:gap-2 md:gap-x-4 lg:grid-cols-3">
-      <div className="shadow-2xl">
+    <article className="grid gap-y-8 sm:grid-cols-2 sm:gap-2 md:gap-x-4 lg:grid-cols-3">
+      <div className="shadow-2xl ">
         <a href="https://react-cocktail-master.netlify.app" target='_blank' ><img src={img1} /></a>
         <div className='flex justify-center py-8 bg-white'>
-          <p className='text-slate-700 tracking-wider'>Cocktail API</p>
+          <p className='text-slate-700 tracking-wider uppercase'>Cocktail API</p>
         </div>
       </div>
-      <div className=" bg-c-2">
+      <div className="shadow-2xl">
         <a href="https://react-swiftstore.netlify.app" target='_blank' >
           <img src={image} />
         </a>
         <div className='flex justify-center py-8 bg-white'>
-          <p className='text-slate-700 tracking-wider'>e-commerce</p>
+          <p className='text-slate-700 tracking-wider uppercase'>e-commerce website</p>
         </div>
       </div>
-      <div className="bg-c-4">
-        container 3
+      <div className="shadow-2xl ">
+        <a href="https://react-swiftstore.netlify.app" target='_blank' >
+          <img src={img3} />
+        </a>
+        <div className='flex justify-center py-8 bg-white'>
+          <p className='text-slate-700 tracking-wider uppercase'>unsplash API</p>
+        </div>
       </div>
     </article>
 
     <div className="flex gap-x-4 mt-8">
-      <button className="bg-c-4 text-gray-100 h-12 w-24 rounded-sm ">Github</button>
-      <button className="bg-c-4 text-gray-100 h-12 w-24 rounded-sm">Projects</button>
+      <button className="bg-c-4 text-gray-100 h-12 w-24 rounded-sm ">
+        <a href="https://github.com/mickealoppong" target='_blank'>Github</a></button>
+      <button className="bg-c-4 text-gray-100 h-12 w-24 rounded-sm">
+        <a href="https://vite-cms.netlify.app" target='_blank'>Projects</a></button>
     </div>
 
   </section>
