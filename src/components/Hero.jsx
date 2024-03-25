59b5f3d2830d1326f0774bd4786562cd2ed7f7d5
yaw oppong <opp.mic@gmail.com>
2024-03-25 10:50:47 +0100

    cdd

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,8 +7,8 @@ const Hero = () => {
 
   return <section className="px-8 mt-8 md:px-16 lg:px-32 bg-gray-300 py-20 ">
 
-    <article className="grid gap-y-8 sm:grid-cols-2 sm:gap-2 md:gap-x-4 lg:grid-cols-3">
-      <div className="shadow-2xl ">
+    <article className="grid gap-y-8 sm:grid-cols-2 sm:gap-2 md:gap-x-4 lg:grid-cols-3 lg:w-9/12 lg:mx-36">
+      <div className="shadow-2xl">
         <a href="https://react-cocktail-master.netlify.app" target='_blank' ><img src={img1} /></a>
         <div className='flex justify-center py-8 bg-white'>
           <p className='text-slate-700 tracking-wider uppercase'>Cocktail API</p>
