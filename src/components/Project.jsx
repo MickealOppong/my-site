f6ae73ba3a9c1cdde1d3d521ce5e36a1ba56a324
yaw oppong <opp.mic@gmail.com>
2024-07-28 19:47:45 +0200

    second

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -16,9 +16,9 @@ const Project = ({ img, text, github, url, description }) => {
         <div className='flex justify-center py-8 bg-white'>
           <p className='text-slate-700 tracking-wider uppercase'>{text}</p>
         </div>
-        <div className={`flex ${showLink ? 'opacity-[100] translate-x-0 duration-300 ' : 'opacity-0 duration-300 -translate-Y-full'} absolute top-[50%] left-[50%] flex-col gap-2  p-2 justify-center items-center `}>
-          <button className='bg-black text-white w-24 capitalize'><a lassName='text-white text-sm ' href={url} target='_blank' >live site</a></button>
-          <button className='bg-black text-white w-24 capitalize'><a className='text-white text-sm ' href={github} target='_blank' >source code</a></button>
+        <div className={`flex ${showLink ? 'opacity-[100] translate-x-0 duration-300 ' : 'opacity-0 duration-300 -translate-Y-full'} absolute top-[50%] left-[45%] flex-col gap-2  p-2 justify-center items-center `}>
+          <button className='bg-c-2 text-white w-24 h-12 capitalize'><a lassName='text-white text-sm ' href={url} target='_blank' >live site</a></button>
+          <button className='bg-c-2 text-white w-24 h-12 capitalize'><a className='text-white text-sm ' href={github} target='_blank' >source code</a></button>
         </div>
       </div>
       <div className="bg-white p-2 text-sm h-[7rem]">
