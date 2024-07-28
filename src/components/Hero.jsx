45e9adba88483cacb3670025e9f5f57f46844c56
yaw oppong <opp.mic@gmail.com>
2024-07-28 13:53:16 +0200

    second

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -34,8 +34,6 @@ const Hero = () => {
     <div className="flex gap-x-4 mt-8 md:16 lg:px-36">
       <button className="bg-c-4 text-gray-100 h-12 w-24 rounded-sm ">
         <a href="https://github.com/mickealoppong" target='_blank'>Github</a></button>
-      <button className="bg-c-4 text-gray-100 h-12 w-24 rounded-sm">
-        <a href="https://vite-cms.netlify.app" target='_blank'>More</a></button>
     </div>
 
   </section>
