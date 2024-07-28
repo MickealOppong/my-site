5ef0eca2ddec6242384c72899e796442a4b89be6
yaw oppong <opp.mic@gmail.com>
2024-07-28 18:14:46 +0200

    second

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -16,13 +16,11 @@ const Project = ({ img, text, github, url }) => {
         <div className='flex justify-center py-8 bg-white'>
           <p className='text-slate-700 tracking-wider uppercase'>{text}</p>
         </div>
-        <div className={` gap-2  p-2 justify-center items-center `}>
+        <div className={`flex flex-col gap-2  p-2 justify-center items-center `}>
           <button className='bg-black text-white w-24 capitalize'><a lassName='text-white text-sm ' href={url} target='_blank' >live site</a></button>
           <button className='bg-black text-white w-24 capitalize'><a className='text-white text-sm ' href={github} target='_blank' >source code</a></button>
         </div>
-
       </div>
-
     </div>
   </article>
 }
