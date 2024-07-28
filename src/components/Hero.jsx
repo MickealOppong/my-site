402c395fcc9a9049b9435e7e4499cb2d0909ee22
yaw oppong <opp.mic@gmail.com>
2024-07-28 14:45:06 +0200

    second

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   //const [image, setImage] = useState(page[1].img)
 
   return <section className="px-4 mt-8 md:px-16 lg:px-32 bg-gray-300 py-20 ">
-
+    {/** FIRST DEMO PROJECT */}
     <article className="grid gap-8 md:gap-12 lg:gap-4 sm:grid-cols-2 sm:gap-2 lg:grid-cols-3 lg:w-10/12 lg:mx-36" >
       <div className="shadow-2xl w-96 md:w-full relative">
         <div >
@@ -15,14 +15,17 @@ const Hero = () => {
           </div>
         </div>
         <div className='bg-white p-2'>
+          <p>Demo project Summary</p>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus distinctio illum eius a, praesentium repellat officiis molestias, rem quam ducimus aperiam corporis. Quaerat molestiae rem provident corporis iste veniam adipisci.
           </p>
         </div>
-        <div className='bg-c-3 hover:scale-105 flex justify-center'>
+        <div className='bg-c-3 hover:scale-105 duration-300 flex justify-center'>
           <a className='text-slate-100' href="https://e-cms.netlify.app/products" target='_blank' >live site</a>
         </div>
       </div>
+
+      {/** SECOND DEMO PROJECT */}
       <div className="shadow-2xl w-96 md:w-full">
         <div>
           <a href="https://react-seahorse.netlify.app/cart" target='_blank' >
@@ -37,10 +40,11 @@ const Hero = () => {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus distinctio illum eius a, praesentium repellat officiis molestias, rem quam ducimus aperiam corporis. Quaerat molestiae rem provident corporis iste veniam adipisci.
           </p>
         </div>
-        <div className='bg-c-3 hover:scale-105 flex justify-center'>
+        <div className='bg-c-3 hover:scale-105  duration-300 flex justify-center'>
           <a className='text-slate-100' href="https://react-seahorse.netlify.app/cart" target='_blank' >live site </a>
         </div>
       </div>
+      {/** THIRD DEMO PROJECT */}
       <div className="shadow-2xl w-96 md:w-full">
         <div>
           <a href="https://seahorse-land-page.netlify.app" target='_blank' >
@@ -55,7 +59,7 @@ const Hero = () => {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus distinctio illum eius a, praesentium repellat officiis molestias, rem quam ducimus aperiam corporis. Quaerat molestiae rem provident corporis iste veniam adipisci.
           </p>
         </div>
-        <div className='bg-c-3 hover:scale-105 flex justify-center'>
+        <div className='bg-c-3 hover:scale-105 duration-300 flex justify-center'>
           <a className='text-slate-100' href="https://seahorse-land-page.netlify.app" target='_blank'>live site </a>
         </div>
       </div>
