d7a2c499397e90543b4043d8e8e078c7385dcde0
yaw oppong <opp.mic@gmail.com>
2024-07-28 16:15:53 +0200

    second

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,9 +16,9 @@ const Hero = () => {
           <div className='flex justify-center py-8 bg-white'>
             <p className='text-slate-700 tracking-wider uppercase'>E-Commerce CMS</p>
           </div>
-          <div className='flex flex-col lg:flex-row gap-2 absolute top-[50%] left-[30%] p-2'>
-            <a className='bg-black text-white w-36' href="https://react-cocktail-master.netlify.app" target='_blank' >live site</a>
-            <a className='bg-black text-white w-36' href="https://react-cocktail-master.netlify.app" target='_blank' >source code</a>
+          <div className='flex flex-col gap-2 absolute top-[50%] left-[30%] p-2'>
+            <a className='bg-black text-white w-24' href="https://react-cocktail-master.netlify.app" target='_blank' >live site</a>
+            <a className='bg-black text-white w-24' href="https://react-cocktail-master.netlify.app" target='_blank' >source code</a>
           </div>
 
         </div>
