d495bd1886ce2bac9148461a09df4d67bfaa7287
yaw oppong <opp.mic@gmail.com>
2024-07-28 19:09:50 +0200

    second

--- src/data.jsx
+++ src/data.jsx
@@ -26,7 +26,7 @@ export const projectData = [
     text: 'e-commerce-cms',
     github: 'https://github.com/MickealOppong/cms-backend-api',
     url: 'https://e-cms.netlify.app/landing',
-    description: 'This is a demo full stack app for creating and managing products,users, and other functionalities needed to operate an e-commerce website. The Backend is written using Java with Spring-boot framework authenticating users using jwt'
+    description: 'This is a demo full stack app for creating and managing products,users, and other functionalities needed to operate an e-commerce website. The Backend is written using Java with Spring-boot framework authenticating users using jwt. The app is 80% completed with few functionality to be completed'
   },
 
   {
@@ -35,7 +35,7 @@ export const projectData = [
     text: 'e-commerce',
     github: 'https://github.com/MickealOppong/store',
     url: 'https://react-seahorse.netlify.app',
-    description: ''
+    description: 'This is a demo front-end app for operating an e-commerce website from customers perspective. This is purely designed from front-end perspective doe demonstration purposes. The app is written using javascript and react framework, with Redux toolkit for state management. This does not include payment functionality'
 
   },
   {
@@ -44,6 +44,6 @@ export const projectData = [
     text: 'e-commerce-landing page',
     github: 'https://github.com/MickealOppong/Fashion-Landing-Page',
     url: 'https://seahorse-land-page.netlify.app',
-    description: ''
+    description: 'This is a demo app for showcasing the Landing page of an e-commerce website. This app is written using vanilla typescript and javascript with no frameworks. The Landing page has basic functionality to capture the basic requirements of a landing page'
   }
 ]
