708674917acb856ad9e79dfdc15c4142094e97b3
yaw oppong <opp.mic@gmail.com>
2024-07-28 18:37:19 +0200

    second

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import Project from './Project';
 const Hero = () => {
   return <section className="px-4 mt-8 md:px-16 lg:px-32 bg-gray-300 py-20 ">
 
-    <div className='flex items-center w-full '>
+    <div className='flex flex-col lg:flex-row gap-x-4 items-center w-full '>
       {
         projectData.map((item) => {
           return <Project img={item.img} key={item.id} />
