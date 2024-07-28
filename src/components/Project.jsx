0d37e4cd51e0edefca50b317cd113ddc4373ec85
yaw oppong <opp.mic@gmail.com>
2024-07-28 19:39:34 +0200

    second

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,7 +4,7 @@ const Project = ({ img, text, github, url, description }) => {
   const [showLink, setShowLink] = useState(false)
   return <article className='w-full'>
     <div className="shadow-2xl w-96 md:w-full
-      " >
+       h-[40rem]" >
       <div className='relative' onMouseEnter={() => {
         setShowLink(() => !showLink)
         console.log('enter');
