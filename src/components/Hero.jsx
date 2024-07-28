43b1438cae6c3aed06c1624b66e2f02374bdbcfd
yaw oppong <opp.mic@gmail.com>
2024-07-28 21:01:25 +0200

    second

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return <section className="max-w-9xl mx-auto px-4 mt-8 md:px-16 lg:px-32 bg-gray-300 py-20 ">
 
     <div className='flex flex-col
- lg:flex-row gap-x-4 items-center w-full'>
+ lg:flex-row gap-4 items-center w-full'>
       {
         projectData.map((item) => {
           return <Project {...item} key={item.id} />
